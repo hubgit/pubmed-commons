74b9e05d51b368efce7a49db96c7ffd0567ea221
Alf Eaton <eaton.alf@gmail.com>
2018-01-12 13:52:04 +0000

    Combine CSV writer and general parser

--- src/parse.js
+++ src/parse.js
@@ -2,6 +2,11 @@ const fs = require('fs-extra')
 const glob = require('glob')
 const path = require('path')
 const cheerio = require('cheerio')
+const moment = require('moment')
+const csvWriteStream = require('csv-write-stream')
+
+const csvWriter = csvWriteStream()
+csvWriter.pipe(fs.createWriteStream(__dirname + '/../data/comments.csv'))
 
 const outputDir = __dirname + '/../data/parsed'
 fs.ensureDirSync(outputDir)
@@ -9,7 +14,19 @@ fs.ensureDirSync(outputDir)
 const inputDir = __dirname + '/../data/source'
 const files = glob.sync(inputDir + '/*.json')
 
+const parseVotes = node => {
+  const matches = node.find('.comm_votes').text().match(/(\d+) of (\d+)/)
+
+  if (!matches) return null
+
+  const [_, positive, total] = matches
+
+  return { positive, total }
+}
+
 files.forEach(file => {
+  const pmid = path.basename(file, '.json')
+
   const data = fs.readJsonSync(file)
 
   $ = cheerio.load(data.comments)
@@ -20,15 +37,26 @@ files.forEach(file => {
   }).map((i, el) => {
     const node = $(el)
 
-    return {
+    const votes = parseVotes(node)
+
+    const item = {
+      pmid,
       id: node.attr('data-cmid'),
-      date: node.find('.comm_date_d').text(),
-      content: node.find('.comm_content').html(),
+      // date: node.find('.comm_date_d').text(),
+      date: moment(node.find('.comm_date_d').text(), 'YYYY MMM DD hh:mm a').toISOString(),
       url: 'http://www.ncbi.nlm.nih.gov' + node.find('.comm_permalink').attr('href'),
-      votes: node.find('.comm_votes').text(),
+      positiveVotes: votes ? votes.positive : 0,
+      totalVotes: votes ? votes.total : 0,
+      moderated: node.find('.not_appr').length,
       author: node.find('.comm_f_name').text(),
-      author_href: node.find('.comm_f_name').attr('href'),
+      authorUrl: 'http://www.ncbi.nlm.nih.gov' + node.find('.comm_f_name').attr('href'),
     }
+
+    csvWriter.write(item)
+
+    item.content = node.find('.comm_content').html()
+
+    return item
   }).toArray()
 
   const output = outputDir + '/' + path.basename(file)
